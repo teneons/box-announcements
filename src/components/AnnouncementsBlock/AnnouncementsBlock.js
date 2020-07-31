45a05fa1a0172ec4c1b7a7ac64eed008b1bccb8a
teneons <teneons@gmail.com>
2020-07-31 18:55:57 +0300

    remove useless block

--- src/components/AnnouncementsBlock/AnnouncementsBlock.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class AnnouncementsBlock extends Component {
-    render() {
-        return (
-            <div className='flex-column w-100 vh-100 bg-danger'></div>
-        )
-    }
-}
