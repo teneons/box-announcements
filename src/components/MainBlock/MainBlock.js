811dfa97747ea36b7a3c1351bbc8b2e999b94c82
teneons <teneons@gmail.com>
2020-08-01 17:23:12 +0300

    include card; fix some moments

--- src/components/MainBlock/MainBlock.js
+++ src/components/MainBlock/MainBlock.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
-import AnnouncementsBlock from '../AnnouncementsBlock/AnnouncementsBlock';
+import CardAnnouncement from '../CardAnnouncement/CardAnnouncement';
 
 export default class MainBlock extends Component {
+
+    AnnouncementEx = {
+        idCard: 1,
+        titleCard: 'Customers a chance chance cha',
+        dateCard: '14.05.2020',
+        descriptionCard: 'We are offering all Doe customers a chance to save up to 45% on computer software this Saturday, March 15, from 8:00 a.m. to 9:00 p.m., at our Main Street store. Receive discounts on all the latest software packages by bringing canned food for the homeless. The more food you bring the more you',
+        contactCard: 'mail@mail.com'
+    }
     
     render() {
         return (
-            <div className='d-flex col-12 vh-60 bg-dark'>
-                <AnnouncementsBlock />
+            <div className='d-flex aling-items-center flex-fill flex-column'>
+
+                <CardAnnouncement idCard={this.AnnouncementEx.idCard}
+                                titleCard={this.AnnouncementEx.titleCard}
+                                descriptionCard={this.AnnouncementEx.descriptionCard}
+                                dateCard={this.AnnouncementEx.dateCard}
+                                contactCard={this.AnnouncementEx.contactCard} />
+                                                <CardAnnouncement idCard={this.AnnouncementEx.idCard}
+                                titleCard={this.AnnouncementEx.titleCard}
+                                descriptionCard={this.AnnouncementEx.descriptionCard}
+                                dateCard={this.AnnouncementEx.dateCard}
+                                contactCard={this.AnnouncementEx.contactCard} />
+                                                <CardAnnouncement idCard={this.AnnouncementEx.idCard}
+                                titleCard={this.AnnouncementEx.titleCard}
+                                descriptionCard={this.AnnouncementEx.descriptionCard}
+                                dateCard={this.AnnouncementEx.dateCard}
+                                contactCard={this.AnnouncementEx.contactCard} />
+
             </div>
         )
     }
