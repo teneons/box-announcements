19dc7e332956cca74f8cb3493d23cd98faac78e9
teneons <teneons@gmail.com>
2020-08-02 01:34:54 +0300

    add min/max letters limits

--- src/components/BtnAdd/ModalAdd.js
+++ src/components/BtnAdd/ModalAdd.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default function ModalAdd () {
 
     const styleInputTitle = {
-        border: 'none',
         fontSize: '3vh'
     }
 
@@ -16,19 +15,19 @@ export default function ModalAdd () {
         let fullDate = `${day}.${month}.${year}`;
         return fullDate;
     }
-    
 
     return (
         <div className="modal fade" id="ModalAdd" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden={false}>
         <div className="modal-dialog modal-dialog-centered">
             <form className="modal-content form-group">
             <div className="modal-header">
-                <input style={styleInputTitle} className="modal-title form-control font-weight-bold" type='text' placeholder='Title announcement' id="exampleModalLabel" />
+                <input className="modal-title form-control font-weight-bold" minLength={3} maxLength={30} style={styleInputTitle} type='text' placeholder='Title announcement' id="exampleModalLabel" />
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div className="modal-body">
                 <span className='card-subtitle text-muted right-align'><i class="fa fa-calendar"></i> <span className='font-weight-bold'>{postDate()}</span></span>
-                <textarea className="form-control mt-2" id="exampleFormControlTextarea1" rows="3" placeholder='Description announcement'></textarea>
+                <textarea className="form-control mt-2" minLength={10} maxLength={295} rows="3" placeholder='Description announcement'></textarea>
+                <input className='col-12 mt-2 inputContact' minLength={7} maxLength={40} type='text' placeholder='Contact data'/>
             </div>
             <div className="modal-footer d-flex justify-content-center">
                 <button type="button" className="btn btn-light" data-dismiss="modal">Close</button>
